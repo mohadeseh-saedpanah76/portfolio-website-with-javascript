8f6762a88723606b2b1f065ccaa6b0d34575cb64
saedpanah <m.saedpanah76@gmail.com>
2022-09-26 13:48:03 +0330

    update app js

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
 const header = document.getElementById('header')
+const modalView = document.querySelector('.services__modal')
+const modalContent = document.querySelectorAll('.services__modal-content')
+const modalCloseBtn = document.querySelectorAll('.services__modal-close')
+const modalShowBtn = document.querySelectorAll('.services__button')
+const workItem = document.querySelectorAll('.work__item')
 
 
 /* =============Change background header ============ */
@@ -7,3 +12,45 @@ const scrollHeader = ()=>{
                  : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll' , scrollHeader)
+
+
+/* ============= Modal Active ============= */
+
+modalShowBtn.forEach((item)=>{
+   item.addEventListener('click', ()=>{
+      modalView.classList.add('active-modal')
+   })
+})
+
+modalCloseBtn.forEach((item)=>{
+   item.addEventListener('click',()=>{
+      modalView.classList.remove('active-modal')
+   })
+})
+
+/* ============ Mixitup Filter Works ============ */
+
+// نگهدارنده ی اصلی استdiv ورودی اول تابع زیر همان 
+
+let filterWorks = mixitup('.work__container' , {
+   selectors : {
+      // مقدار تارگت میشود کلاس تگ هایی که فیلتر روی آنها باید انجام شود
+      target: '.work__card'
+   },
+   animation : {
+      duration : 300
+   }
+});
+
+/* active work */
+function activeWork (){
+   workItem.forEach((item)=>{
+      item.classList.remove('active-work')
+   })
+   this.classList.add('active-work')
+}
+
+workItem.forEach((item)=>{
+   item.addEventListener('click' , activeWork)
+})
+
